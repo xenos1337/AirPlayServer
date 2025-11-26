65dd02334d72081295a1e2e9f320a7f4d71da948
xenos <66328734+xenos1337@users.noreply.github.com>
2025-11-27 00:41:46 +0100

    Update GLOBAL_MODEL in global.h to reflect the new Apple TV model identifier "AppleTV14,1".
    Signed-off-by: xenos <66328734+xenos1337@users.noreply.github.com>

--- airplay2/lib/global.h
+++ airplay2/lib/global.h
@@ -2,7 +2,7 @@
 #define GLOBAL_H
 
 #define GLOBAL_FEATURES 0x7
-#define GLOBAL_MODEL    "AppleTV3,2"
+#define GLOBAL_MODEL    "AppleTV14,1"
 #define GLOBAL_VERSION  "220.68"
 
 #define MAX_HWADDR_LEN 6
