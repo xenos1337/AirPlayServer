ba4453e25e604edc6b7808a05f3e8aa5cb805736
xenos <66328734+xenos1337@users.noreply.github.com>
2026-02-06 17:38:14 +0100

    fix(ui): switch to Windows subsystem to hide console window

--- AirPlayServer/AirPlayServer.cpp
+++ AirPlayServer/AirPlayServer.cpp
@@ -3,7 +3,6 @@
 // Window shows home screen with ImGui UI.
 //
 #include <windows.h>
-#include <iostream>
 #include <signal.h>
 #include "Airplay2Head.h"
 #include "CAirServerCallback.h"
@@ -22,48 +21,10 @@ void CleanupAndShutdown()
     }
     g_bShuttingDown = true;
 
-    printf("Initiating graceful shutdown...\n");
     if (g_pPlayer != NULL) {
         g_pPlayer->m_server.stop();
-        // Give time for server to stop completely
         Sleep(150);
     }
-}
-
-// Console control handler for Ctrl+C, Ctrl+Break, console close, etc.
-BOOL WINAPI ConsoleCtrlHandler(DWORD ctrlType)
-{
-    switch (ctrlType) {
-        case CTRL_C_EVENT:
-            printf("\nCtrl+C received.\n");
-            break;
-        case CTRL_BREAK_EVENT:
-            printf("\nCtrl+Break received.\n");
-            break;
-        case CTRL_CLOSE_EVENT:
-            printf("\nConsole close event received.\n");
-            break;
-        case CTRL_LOGOFF_EVENT:
-            printf("\nUser logoff event received.\n");
-            break;
-        case CTRL_SHUTDOWN_EVENT:
-            printf("\nSystem shutdown event received.\n");
-            break;
-        default:
-            return FALSE;
-    }
-
-    CleanupAndShutdown();
-
-    // Post quit event to SDL to exit the event loop
-    SDL_Event quitEvent;
-    quitEvent.type = SDL_QUIT;
-    SDL_PushEvent(&quitEvent);
-
-    // Give some time for cleanup to complete
-    Sleep(500);
-
-    return TRUE;
 }
 
 // atexit handler as a fallback
@@ -72,14 +33,9 @@ void AtExitHandler()
     CleanupAndShutdown();
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
-    // Register console control handler
-    if (!SetConsoleCtrlHandler(ConsoleCtrlHandler, TRUE)) {
-        printf("Warning: Failed to set console control handler\n");
-    }
 
     // Register atexit handler as fallback
     atexit(AtExitHandler);
